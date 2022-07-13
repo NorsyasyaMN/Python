1f0db89eec73bbed24bca5349711615b53d4aa3a
cat201Project <cat201project@gmail.com>
2022-07-13 10:15:17 +0800

    message

--- cpt212ass/CPT212.py
+++ cpt212ass/CPT212.py
@@ -5,6 +5,7 @@ from turtle import distance
 import sys # Library for INT_MAX
 # remove later just for troubleshooting purposes
 
+
 # Class to represent graph and all the functions
 class Graph:
     """ Constructor """
@@ -207,7 +208,7 @@ class Graph:
         for node in self.adjList.keys():
             print(node, "->", self.adjList[node])
         print()
-        print(self.adjList["GLA"].items())
+        print(self.adjList["SE"].items())
         print(len(self.adjList))
     
     def is_path(t, path):
@@ -442,63 +443,14 @@ class Graph:
 
     """-----------------Function 4: Finding Minimum Spanning Tree--------------"""
 
-    def PrimMST(self):
-        # Get the number of vertices in graph
-        V = self.V
- 
-        # key values used to pick minimum weight edge in cut
-        key = []
- 
-        # List to store constructed MST
-        parent = []
- 
-        # minHeap represents set E
-        minHeap = Heap()
- 
-        # Initialize min heap with all vertices. Key values of all
-        # vertices (except the 0th vertex) is is initially infinite
-        for v in range(V):
-            parent.append(-1)
-            key.append(1e7)
-            minHeap.array.append(minHeap.newMinHeapNode(v, key[v]))
-            minHeap.pos.append(v)
- 
-        # Make key value of 0th vertex as 0 so
-        # that it is extracted first
-        minHeap.pos[0] = 0
-        key[0] = 0
-        minHeap.decreaseKey(0, key[0])
- 
-        # Initially size of min heap is equal to V
-        minHeap.size = V
- 
-        # In the following loop, min heap contains all nodes
-        # not yet added in the MST.
-        while minHeap.isEmpty() == False:
- 
-            # Extract the vertex with minimum distance value
-            newHeapNode = minHeap.extractMin()
-            u = newHeapNode[0]
- 
-            # Traverse through all adjacent vertices of u
-            # (the extracted vertex) and update their
-            # distance values
-            for pCrawl in self.graph[u]:
- 
-                v = pCrawl[0]
- 
-                # If shortest distance to v is not finalized
-                # yet, and distance to v through u is less than
-                # its previously calculated distance
-                if minHeap.isInMinHeap(v) and pCrawl[1] < key[v]:
-                    key[v] = pCrawl[1]
-                    parent[v] = u
- 
-                    # update distance value in min heap also
-                    minHeap.decreaseKey(v, key[v])
- 
-        printArr(parent, V)
-
+    # Dijkstra algorithm to find shortest path
+    def dijkstra(self, src, end):
+        for node in self.adjList:
+            if node == end:
+                if node not in self.adjList[src].items():
+                    self.add_newRandomEdge()
+            else:
+                break
 # Function to clear screen
 def cls_screen():
 
